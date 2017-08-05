68d059917ecf11d965e6d4c660721b7c163602b3
Sagicary <sagicary@gmail.com>
2017-08-04 21:21:45 -0400

    Cambio a bootstrap, nav y fondo

--- gulpfile.js
+++ gulpfile.js
@@ -5,13 +5,13 @@ var sass = require('gulp-sass');
 var minifyCSS = require('gulp-minify-css');
 var webserver = require('gulp-webserver');
 gulp.task('script', function(){
-	gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/materialize-css/dist/js/materialize.js', 'assets/js/*.js'])
+	gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/bootstrap/dist/js/bootstrap.js', 'assets/js/*.js'])
 		.pipe(concat('script.js'))
 		//carpeta dist
 		.pipe(gulp.dest('dist/js/'));
 })
 gulp.task('style', function(){
-	gulp.src(['node_modules/materialize-css/dist/css/materialize.css', 'assets/sass/main.scss'])
+	gulp.src(['node_modules/bootstrap/dist/css/bootstrap.css', 'assets/sass/main.scss'])
 		.pipe(sass().on('error', sass.logError))
 		.pipe(minifyCSS())
 		.pipe(concat('style.min.css'))
